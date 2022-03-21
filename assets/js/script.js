f4334301c729955f860690417b6f3eb5c90fc93a
itisahmedh <ahmed_hyder@outlook.com>
2022-03-21 16:27:46 +0000

    Add match checking function for cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,9 +11,59 @@ const cards = $(".card");
 /*
 *Function for flipping cards
 */
+let hasFlippedcard = false;
+let firstCard, secondCard;
 function flipCard() {
-    this.classList.toggle('flip');
+    this.classList.add('flip');
+
+    // Check if this the first card to flip
+if (!hasFlippedcard) {
+    hasFlippedcard = true;
+    firstCard = this;
+
+    
+    return; }
+
+        hasFlippedcard = false;
+        secondCard = this;
+        // Check if the card are the same
+        
+        checkForMatch();
+    
 }
+/*
+*Check if the cards are the same
+*
+*/
+function checkForMatch() {
+    if(firstCard.dataset.framework === secondCard.dataset.framework) {
+        disableCards();
+    } else {
+        // If it is not a match
+        unflipCards();
+}
+
+/*
+* Disable cards from flipping
+*/
+function disableCards() {
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click',flipCard);
+}
+
+/*
+* Flip the cards back if they are not the same
+*/
+function unflipCards() {
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+        }, 1500);
+    }
+}
+
+
 for (let card of cards) {
     card.addEventListener('click',flipCard);
 }
+
