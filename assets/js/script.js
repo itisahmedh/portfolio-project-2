6faac7ce09b7c70cb1623d073a9dfa4931c63c33
itisahmedh <ahmed_hyder@outlook.com>
2022-03-21 18:30:42 +0000

    Add Score and Alert

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,12 +8,19 @@ $('#how-btn').click(function(){
 // Game
 const cards = $(".card");
 
-/*
-*Function for flipping cards
-*/
+/**
+ * Function for flipping cards
+ */
+
+shuffle();
 let hasFlippedcard = false;
+let lockBoard = false
 let firstCard, secondCard;
+let oldScore = parseInt(document.getElementById('score').innerText);
+let incorrect = parseInt(document.getElementById('incorrect').innerText);
 function flipCard() {
+    if(lockBoard) return;
+    if(this === firstCard) return;
     this.classList.add('flip');
 
     // Check if this the first card to flip
@@ -31,36 +38,86 @@ if (!hasFlippedcard) {
         checkForMatch();
     
 }
-/*
-*Check if the cards are the same
-*
-*/
+/**
+ * Check if the cards are the same
+ */
 function checkForMatch() {
     if(firstCard.dataset.framework === secondCard.dataset.framework) {
+        incrementScore();
         disableCards();
     } else {
         // If it is not a match
+        incrementIncorrect();
         unflipCards();
 }
 
-/*
-* Disable cards from flipping
-*/
+/**
+ * Disable cards from flipping
+ */
 function disableCards() {
     firstCard.removeEventListener('click', flipCard);
     secondCard.removeEventListener('click',flipCard);
 }
 
-/*
-* Flip the cards back if they are not the same
-*/
+/**
+ * Flip the cards back if they are not the same
+ */
 function unflipCards() {
+    lockBoard = true;
     setTimeout(() => {
         firstCard.classList.remove('flip');
         secondCard.classList.remove('flip');
+
+        lockBoard = false;
+        
+        resetBoard();
         }, 1500);
     }
 }
+
+/** 
+ * Resets the board
+*/
+function resetBoard() {
+    [hasFlippedcard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
+}
+
+/**
+ * Shuffle the cards
+ */
+function shuffle() {
+    for (let x of cards) {
+        let randomPos = Math.floor(Math.random() *12);
+        x.style.order = randomPos;
+    }
+}
+
+
+/**
+ * Increases the score by one
+ */
+function incrementScore() {
+    
+    document.getElementById('score').innerText = ++oldScore;
+    if (oldScore === 6) {
+        alert('Congrats!')
+    }
+}
+
+/**
+ * Get the incorrect score and increases it by 1
+ */
+function incrementIncorrect() {
+    
+    document.getElementById('incorrect').innerText = ++incorrect;
+    if (incorrect === 7) {
+        alert('noooooooooo!')
+    }
+}
+
+
+
 
 
 for (let card of cards) {
