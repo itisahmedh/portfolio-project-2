b95fdaac5bd9ccc323be4d786aaed861208e33eb
itisahmedh <ahmed_hyder@outlook.com>
2022-03-21 15:48:19 +0000

    Add images and 3d affect

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,16 @@
 $('#how-btn').click(function(){
     $('#how').toggle('slow');
 });
+
+// Game
+const cards = $(".card");
+
+/*
+*Function for flipping cards
+*/
+function flipCard() {
+    this.classList.toggle('flip');
+}
+for (let card of cards) {
+    card.addEventListener('click',flipCard);
+}
